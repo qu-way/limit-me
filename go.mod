ba7f9cc1bfabf088cd2ee6721694adfce3c59057
qu-way <codeninja@foxmail.com>
2020-05-17 01:52:53 +0800

    (\ /) (O.o) (> <) Bunny approves these changes.

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/qu-way/limit-me
 go 1.14
 
 require (
+	github.com/kimsudo/wulimt v0.0.0-20200516100443-f0fcf9cda9ba
 	github.com/logcfg/getzap v0.0.0-20200429091553-1b86d84e4c8e
 	go.uber.org/zap v1.15.0
 )
--- play.go
+++ play.go
@@ -2,7 +2,9 @@ package main
 
 import (
 	"os"
+	"time"
 
+	wl "github.com/kimsudo/wulimt"
 	"github.com/logcfg/getzap"
 	"go.uber.org/zap"
 )
@@ -18,8 +20,18 @@ func init() {
 func main() {
 	pwd, _ := os.Getwd()
 	host, _ := os.Hostname()
-	log.Infow("hello world",
+	log.Debugw("hello world",
 		"host", host,
 		"pwd", pwd,
 	)
+
+	r := wl.GetOrParseNew("hello", "2-5s")
+	last := time.Now()
+	for i := 1; i <= 10; i++ {
+		r.Wait()
+
+		cur := time.Now()
+		log.Infow("approved to run", "index", i, "interval", cur.Sub(last))
+		last = cur
+	}
 }
