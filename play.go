3e07e0e63a58d397a6d0b46961a76200d7de4668
qu-way <codeninja@foxmail.com>
2020-05-17 17:12:29 +0800

    wait by myself

--- play.go
+++ play.go
@@ -31,8 +31,10 @@ func main() {
 
 	// test1()
 	// test2()
-	test3()
+	// test3()
 	// test4()
+	// test5()
+	test6()
 }
 
 func test1() {
@@ -163,6 +165,108 @@ func test4() {
 			dataC <- i
 			dataC <- i + 1
 		}
+	}
+
+	dataC <- 0
+	<-doneC
+
+	end := time.Now()
+	log.Debugw("all is done", "time_cost", end.Sub(start))
+}
+
+func test5() {
+	dataC := make(chan int, 10)
+	doneC := make(chan bool)
+
+	start := time.Now()
+
+	last := time.Now()
+	go func(c <-chan int) {
+		for {
+			d := <-c
+			cur := time.Now()
+			if d <= 0 {
+				log.Warnw("got signal to stop", "data", d)
+				break
+			}
+
+			log.Infow("task to run", "data", d, "interval", cur.Sub(last))
+			last = cur
+		}
+		doneC <- true
+
+	}(dataC)
+
+	r := xr.NewLimiter(xr.Every(time.Duration(float64(time.Second)*2.5)), 1)
+	// ctx := context.TODO()
+	// ctx, _ := context.WithTimeout(context.Background(), 11*time.Second)
+
+	for i := 1; i <= 10; i++ {
+		for !r.Allow() {
+			log.Debugw("not allowed, waiting", "index", i)
+			time.Sleep(250 * time.Millisecond)
+		}
+		log.Debugw("approved to send", "index", i)
+		dataC <- i
+
+		// if err := r.Wait(ctx); err != nil {
+		// 	log.Warnw("got limiter error", "index", i, zap.Error(err))
+		// } else {
+		// 	log.Debugw("approved to send", "index", i)
+		// 	dataC <- i
+		// }
+	}
+
+	dataC <- 0
+	<-doneC
+
+	end := time.Now()
+	log.Debugw("all is done", "time_cost", end.Sub(start))
+}
+
+func test6() {
+	dataC := make(chan int, 10)
+	doneC := make(chan bool)
+
+	start := time.Now()
+
+	last := time.Now()
+	go func(c <-chan int) {
+		for {
+			d := <-c
+			cur := time.Now()
+			if d <= 0 {
+				log.Warnw("got signal to stop", "data", d)
+				break
+			}
+
+			log.Infow("task to run", "data", d, "interval", cur.Sub(last))
+			last = cur
+		}
+		doneC <- true
+
+	}(dataC)
+
+	r := xr.NewLimiter(xr.Every(time.Duration(float64(time.Second)*2.5)), 1)
+	// ctx := context.TODO()
+	// ctx, _ := context.WithTimeout(context.Background(), 11*time.Second)
+
+	for i := 1; i <= 10; i++ {
+		rev := r.Reserve()
+		if !rev.OK() {
+			log.Warnw("got limiter error: reserve not ok")
+			continue
+		}
+
+		delay := rev.Delay()
+		if delay > 0 {
+			log.Debugw("not allowed, waiting", "index", i, "interval", delay)
+			<-time.After(delay)
+			log.Debugw("time is up, and let's do it")
+		}
+
+		log.Debugw("approved to send", "index", i)
+		dataC <- i
 	}
 
 	dataC <- 0
